f9d48513020ab6f8b8ea0c5831746c3268f0e1cf
bugbuster <bugbuster@bugbuster.dev>
2024-04-01 15:12:50 +0200

    remove unused dirs

--- keyboards/keychron/q3_max/rules.mk
+++ keyboards/keychron/q3_max/rules.mk
@@ -1,4 +1,5 @@
 include keyboards/keychron/common/wireless/wireless.mk
 include keyboards/keychron/common/keychron_common.mk
+include keyboards/keychron/firmata/firmata.mk
 
 VPATH += $(TOP_DIR)/keyboards/keychron
