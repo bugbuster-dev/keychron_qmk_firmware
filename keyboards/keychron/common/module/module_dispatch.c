5a10583c18d4557e9a24db10307c6ef9374c8026
bug buster <bugbuster@bugbuster.dev>
2026-04-20 18:16:27 +0200

    feat(module): add combo hook dispatchers for QMK callback delegation

---
+++ keyboards/keychron/common/module/module_dispatch.c
@@ -0,0 +1,49 @@
+/*
+    Module Dispatch - Implementation
+    Provides dispatcher functions for QMK callbacks that can be overridden by modules.
+*/
+
+#include "module_dispatch.h"
+#include "module_loader.h"
+
+/* Helper typedefs for hook functions */
+typedef bool (*combo_should_trigger_fn)(uint16_t, combo_t*, uint16_t, keyrecord_t*);
+typedef void (*process_combo_event_fn)(uint16_t, bool);
+typedef uint16_t (*get_combo_term_fn)(uint16_t, combo_t*);
+
+bool module_dispatch_combo_should_trigger(uint16_t combo_index, combo_t *combo,
+                                          uint16_t keycode, keyrecord_t *record) {
+    module_hook_entry_t* hooks = module_get_hook_table();
+    uint8_t hook_count = module_get_hook_count();
+
+    if (combo_index < hook_count && hooks[MODULE_HOOK_COMBO_SHOULD_TRIGGER].func != NULL) {
+        combo_should_trigger_fn fn = (combo_should_trigger_fn)hooks[MODULE_HOOK_COMBO_SHOULD_TRIGGER].func;
+        return fn(combo_index, combo, keycode, record);
+    }
+
+    /* Default: all combos trigger */
+    return true;
+}
+
+void module_dispatch_process_combo_event(uint16_t combo_index, bool pressed) {
+    module_hook_entry_t* hooks = module_get_hook_table();
+    uint8_t hook_count = module_get_hook_count();
+
+    if (combo_index < hook_count && hooks[MODULE_HOOK_PROCESS_COMBO_EVENT].func != NULL) {
+        process_combo_event_fn fn = (process_combo_event_fn)hooks[MODULE_HOOK_PROCESS_COMBO_EVENT].func;
+        fn(combo_index, pressed);
+    }
+}
+
+uint16_t module_dispatch_get_combo_term(uint16_t index, combo_t *combo) {
+    module_hook_entry_t* hooks = module_get_hook_table();
+    uint8_t hook_count = module_get_hook_count();
+
+    if (index < hook_count && hooks[MODULE_HOOK_GET_COMBO_TERM].func != NULL) {
+        get_combo_term_fn fn = (get_combo_term_fn)hooks[MODULE_HOOK_GET_COMBO_TERM].func;
+        return fn(index, combo);
+    }
+
+    /* Default: use COMBO_TERM */
+    return COMBO_TERM;
+}
---
+++ keyboards/keychron/common/module/module_dispatch.h
@@ -0,0 +1,40 @@
+/*
+    Module Dispatch - Header
+    Provides dispatcher functions for QMK callbacks that can be overridden by modules.
+*/
+
+#pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "quantum/combo.h"
+#include "quantum/keycodes.h"
+#include "keyevent.h"
+
+/* Dispatcher function prototypes */
+
+/**
+ * @brief Dispatcher for combo_should_trigger.
+ * @param combo_index The index of the combo.
+ * @param combo The combo definition.
+ * @param keycode The keycode being processed.
+ * @param record The keyrecord.
+ * @return true if the combo should trigger, false otherwise.
+ */
+bool module_dispatch_combo_should_trigger(uint16_t combo_index, combo_t *combo,
+                                          uint16_t keycode, keyrecord_t *record);
+
+/**
+ * @brief Dispatcher for process_combo_event.
+ * @param combo_index The index of the combo.
+ * @param pressed true if the combo was pressed, false if released.
+ */
+void module_dispatch_process_combo_event(uint16_t combo_index, bool pressed);
+
+/**
+ * @brief Dispatcher for get_combo_term.
+ * @param index The combo index.
+ * @param combo The combo definition.
+ * @return The combo term in milliseconds.
+ */
+uint16_t module_dispatch_get_combo_term(uint16_t index, combo_t *combo);
