e2ad6693c8eac00983e7e71ec52fc1cb8abc8aff
bug buster <bugbuster@bugbuster.dev>
2026-04-28 22:03:07 +0200

    common: unify dynamic combo/tap dance/leader max slots to 10

--- keyboards/keychron/common/combo/eeconfig_combo.h
+++ keyboards/keychron/common/combo/eeconfig_combo.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define COMBO_DEF_MAX_KEYS 8
-#define COMBO_DEF_MAX_SLOTS 16
+#define COMBO_DEF_MAX_SLOTS 10
 
-// 1 byte magic + 16 * 18 bytes = 289 bytes
+// 1 byte magic + 10 * 18 bytes = 181 bytes
 #define EECONFIG_SIZE_COMBO (1 + COMBO_DEF_MAX_SLOTS * (COMBO_DEF_MAX_KEYS * 2 + 2))
--- keyboards/keychron/common/tap_dance/eeconfig_tap_dance.h
+++ keyboards/keychron/common/tap_dance/eeconfig_tap_dance.h
@@ -1,7 +1,7 @@
 // keyboards/keychron/common/tap_dance/eeconfig_tap_dance.h
 #pragma once
 
-#define TAP_DANCE_DEF_MAX_SLOTS  8
+#define TAP_DANCE_DEF_MAX_SLOTS  10
 
-// 1 byte magic + 8 slots × 8 bytes (4 × uint16_t) = 65 bytes
+// 1 byte magic + 10 slots × 8 bytes (4 × uint16_t) = 81 bytes
 #define EECONFIG_SIZE_TAP_DANCE  (1 + TAP_DANCE_DEF_MAX_SLOTS * 8)
